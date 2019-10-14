6bf51c2a889467c0a12d9ed5bd70e0ea90c4cbb3
Hao Wu <echowuhao@gmail.com>
2019-10-14 19:32:54 +0800

    eslint

---
+++ tests/unit/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    env: {
+        jest: true
+    },
+    extends: [
+        // add more generic rulesets here, such as:
+        // 'eslint:recommended',
+        'plugin:vue/recommended'
+      ],
+      rules: {
+        // override/add rules settings here, such as:
+        // 'vue/no-unused-vars': 'error'
+      }
+}
